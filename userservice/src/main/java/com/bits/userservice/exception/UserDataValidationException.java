038c4827f180917c764143e175c975a78ea39736
charu8003 <166981295+charu8003@users.noreply.github.com>
2024-11-21 11:34:28 +0530

    Delete userservice directory

--- userservice/src/main/java/com/bits/userservice/exception/UserDataValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.bits.userservice.exception;
-
-public class UserDataValidationException extends RuntimeException {
-    public UserDataValidationException(String message) {
-        super(message);
-    }
-}
